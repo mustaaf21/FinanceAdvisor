b133904c33ed5326c2126b1954182196388f8823
mustaaf21 <mustafeezk11@gmail.com>
2026-03-31 16:54:46 +0530

    Add UserSessions migration

---
+++ src/FinanceAdvisor.Infrastructure/Migrations/20260331112113_AddUserSessions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceAdvisor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSessions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "GJ8rPg/3H31Q5qw8DVkY1y9gIT7j/yD2XlK6wcfik4I=");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "hashedpassword");
+        }
+    }
+}
